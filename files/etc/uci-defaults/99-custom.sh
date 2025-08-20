c41c2e25f980ed0f18275a28cc1c1e61531f57dd
hfy2025 <hongfy2025@gmail.com>
2025-08-20 16:14:04 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-# Set hostname
-uci set system.@system[0].hostname='MyRouter'  # Modify here
-uci set system.@system[0].description='Custom OpenWrt Firmware'
+# 设置主机名
+uci set system.@system[0].hostname='NIT'  # 可修改
+uci set system.@system[0].description='04543473 OpenWRT'
 uci commit system
 
-# Set management IP
-uci set network.lan.ipaddr='192.168.6.1'  # Modify here
+# 设置后台管理 IP
+uci set network.lan.ipaddr='192.168.6.1'  # 可修改
 uci set network.lan.netmask='255.255.255.0'
 uci commit network
 
-# Set default theme to argon
-uci set luci.main.mediaurlbase='/luci-static/argon'  # Modify if needed
+# 设置默认主题为 argon
+uci set luci.main.mediaurlbase='/luci-static/argon'  # 可修改
 uci commit luci
 
-# Custom signature/motd (optional, append to banner)
-echo "Custom Signature: Built by Grok on $(date)" >> /etc/banner  # Modify signature here
-
-# Other customizations (e.g., enable services)
+# 自定义签名（附加到 banner）
+echo "自定义签名: 由 Grok 构建于 $(date)" >> /etc/banner  # 可修改
 exit 0
