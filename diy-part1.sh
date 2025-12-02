91b246fe66bba63ef126528ec7157c9b15aa4052
hfy2025 <hongfy2025@gmail.com>
2025-12-02 10:09:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,12 @@
-# Add third-party feeds for extended packages
-# Kenzok8 (OpenClash, Nikki, HomeProxy, etc.)
+# 添加第三方插件源 (Feeds)
+# Kenzok8 (OpenClash, Nikki, HomeProxy 等)
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# iStore and NAS packages (Quickstart, iStore)
+# iStore 和 NAS 相关 (Quickstart, iStore)
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 
-# Argon Theme (Manual Clone)
+# Argon 主题 (手动克隆)
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
