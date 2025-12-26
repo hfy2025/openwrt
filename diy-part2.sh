a65a5750940be02cf784e443cda4d8613f063c87
hfy2025 <hongfy2025@gmail.com>
2025-12-26 14:22:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,35 +2,39 @@
 
 echo "开始添加第三方插件..."
 
-cd openwrt-source
+cd ${{ github.workspace }}/openwrt
 
-# 添加第三方软件源
-cat > feeds.conf << 'EOF'
-src-git official https://git.openwrt.org/feed/packages.git^6c1c8e6
-src-git luci https://git.openwrt.org/project/luci.git^e6b8b4c
-src-git routing https://git.openwrt.org/feed/routing.git^e63b254
-src-git telephony https://git.openwrt.org/feed/telephony.git^9e0c7c8
+# 首先更新一次官方 feeds
+./scripts/feeds update -a
+
+# 添加第三方软件源到 feeds.conf.default
+cat >> feeds.conf.default << 'EOF'
 
 # 第三方插件源
-src-git small https://github.com/kenzok8/small-package.git^d5b7bf0
-src-git openclash https://github.com/vernesong/OpenClash.git^f9193ff
-src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git^0b5ae66
-src-git dockerman https://github.com/lisaac/luci-app-dockerman.git^d3a9c29
+src-git small https://github.com/kenzok8/small-package
+src-git openclash https://github.com/vernesong/OpenClash
+src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome
+src-git dockerman https://github.com/lisaac/luci-app-dockerman
+src-git argon_config https://github.com/jerrykuku/luci-app-argon-config
+src-git store https://github.com/linkease/istore
 EOF
 
-# 更新并安装 feeds
+# 再次更新所有 feeds
 ./scripts/feeds update -a
+
+# 安装所有包
 ./scripts/feeds install -a
 
-# 为某些插件创建必要的符号链接
-mkdir -p package/community
-cd package/community
-
-# 克隆一些额外的插件
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
-git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git
-
-cd ../..
+# 特别安装一些必要的包
+./scripts/feeds install luci-app-adguardhome
+./scripts/feeds install luci-app-openclash
+./scripts/feeds install luci-app-dockerman
+./scripts/feeds install luci-app-argon-config
+./scripts/feeds install luci-app-store
+./scripts/feeds install luci-app-turboacc
+./scripts/feeds install luci-app-samba4
+./scripts/feeds install luci-app-upnp
+./scripts/feeds install luci-app-appfilter
+./scripts/feeds install luci-app-quickstart
 
 echo "第三方插件添加完成！"
